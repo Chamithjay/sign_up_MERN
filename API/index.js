3f8a014a28b704d6e199070ea1a5152146d9d7f0
Chamitha Thambiliyagoda <chamithajayavihan2@gmail.com>
2024-06-23 22:26:28 +0530

    MERN

--- API/index.js
+++ API/index.js
@@ -6,6 +6,6 @@ const multer=require('multer');
 var app=express();
 app.use(cors());
 
-var ConnectionString='mongodb+srv://admin:lxl43Z5ZMK94DCx2@cluster0.7i0ra1o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0mongodb://localhost:27017';
+var ConnectionString='mongodb+srv://admin:<password>@cluster0.7i0ra1o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0mongodb://localhost:27017';
 
 var DatabaseName='sign_up';
